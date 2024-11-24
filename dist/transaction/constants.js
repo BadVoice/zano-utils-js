4b95a433fa4c1040587e95cfc010b807192a3a6e
German <german.suvorov134@gmail.com>
2024-11-24 19:33:27 +0300

    chore(release): version 0.0.1

---
+++ dist/transaction/constants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CRYPTO_HDS_OUT_CONCEALING_POINT = exports.CRYPTO_HDS_OUT_AMOUNT_MASK = void 0;
+exports.CRYPTO_HDS_OUT_AMOUNT_MASK = Buffer.from('ZANO_HDS_OUT_AMOUNT_MASK_______\0', 'ascii');
+exports.CRYPTO_HDS_OUT_CONCEALING_POINT = Buffer.from('ZANO_HDS_OUT_CONCEALING_POINT__\0', 'ascii');
+//# sourceMappingURL=constants.js.map
