80f183589c8fcb49e0b6cdaafb7381ec87f5f6e7
german.s <german.s@changenow.io>
2025-01-10 21:21:49 +0300

    feat: pre seed phrase

--- src/test.ts
+++ src/test.ts
@@ -9,10 +9,8 @@ void (async() => {
 
 
   // stomach stick box coward universe grade blonde wait admire release screen flame rope accident creak warrior clue ahead mom embarrass season support shown above burst problem
-  // 7feda06581b01d19678f2bc4fd4b4d3d9920eb05eaafa6d5a34139bd8a3e0a04
 
 
-  // 1f0142e527927af7600edee66ed94426531a1635ca12e4db8dee5abf0a9caa25
   // save tea dwell crap family moment possibly certainly skill climb snow clay ship bought goose forgot natural footstep crush plain pride cease vain agony children curse
   // const seed = await accUtils.restoreSeedPhrase(
   //   'save tea dwell crap family moment possibly certainly skill climb snow clay ship bought goose forgot natural footstep crush plain pride cease vain agony children curse', '');
