5409706500c425c12335ebe7fc786539c7298913
german.s <german.s@changenow.io>
2025-01-11 18:39:58 +0300

    chore(build): actual dist for 0.3.6 version

--- dist/core/helpers.js
+++ dist/core/helpers.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.reduceScalar = exports.encodePoint = exports.decodeScalar = exports.squareRoot = exports.decodeInt = void 0;
+exports.encodeInt = exports.decodePoint = exports.reduceScalar = exports.encodePoint = exports.decodeScalar = exports.squareRoot = exports.decodeInt = void 0;
 const buffer_1 = require("buffer");
 const bn_js_1 = __importDefault(require("bn.js"));
 const crypto_data_1 = require("./crypto-data");
@@ -35,4 +35,35 @@ function reduceScalar(scalar, curveOrder) {
     return scalar.mod(curveOrder);
 }
 exports.reduceScalar = reduceScalar;
+function decodePoint(buf, message = 'Invalid point') {
+    const xIsOdd = (buf[buf.length - 1] & 0x80) !== 0;
+    buf[buf.length - 1] = buf[buf.length - 1] & ~0x80;
+    let y = decodeInt(buf);
+    if (y.gte(crypto_data_1.ec.curve.p)) {
+        throw new RangeError(message);
+    }
+    y = y.toRed(crypto_data_1.ec.curve.red);
+    const y2 = y.redSqr();
+    const u = y2.redSub(crypto_data_1.ec.curve.c2);
+    const v = y2.redMul(crypto_data_1.ec.curve.d).redMul(crypto_data_1.ec.curve.c2).redSub(crypto_data_1.ec.curve.a);
+    let x = squareRoot(u, v);
+    if (!u.redSub(x.redSqr().redMul(v)).isZero()) {
+        x = x.redMul(crypto_data_1.sqrtm1);
+        if (!u.redSub(x.redSqr().redMul(v)).isZero()) {
+            throw new RangeError(message);
+        }
+    }
+    if (x.fromRed().isZero() && xIsOdd) {
+        throw new RangeError(message);
+    }
+    if (x.fromRed().isOdd() !== xIsOdd) {
+        x = x.redNeg();
+    }
+    return crypto_data_1.ec.curve.point(x, y);
+}
+exports.decodePoint = decodePoint;
+function encodeInt(num) {
+    return num.toArrayLike(buffer_1.Buffer, 'le', 32);
+}
+exports.encodeInt = encodeInt;
 //# sourceMappingURL=helpers.js.map
