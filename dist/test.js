c602113ff7abb3aaddb08adf16d61f80670ea5ce
german.s <german.s@changenow.io>
2025-01-13 18:42:41 +0300

    chore(build): actual dist version 0.3.8

--- dist/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const zano_account_utils_1 = require("./account/zano-account-utils");
-void (async () => {
-    const accUtils = new zano_account_utils_1.ZanoAccountUtils();
-    const account = await accUtils.generateAccount();
-    console.log(account);
-})();
-//# sourceMappingURL=test.js.map
